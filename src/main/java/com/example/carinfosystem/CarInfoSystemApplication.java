6dcbcb237e6968f2af30a5b0c756dc61ece4dacb
Yana Ilieva <yanailieva199@gmail.com>
2021-05-15 15:48:05 +0300

    First Commit

---
+++ src/main/java/com/example/carinfosystem/CarInfoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.carinfosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInfoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInfoSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carinfosystem/CarInfoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carinfosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInfoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
