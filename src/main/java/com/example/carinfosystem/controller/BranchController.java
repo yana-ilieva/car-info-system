1e8e5b032beeffc7443af1b7cada45332186b028
Yana Ilieva <yanailieva199@gmail.com>
2021-05-29 13:10:24 +0300

    method for finding branches by city added

--- src/main/java/com/example/carinfosystem/controller/BranchController.java
+++ src/main/java/com/example/carinfosystem/controller/BranchController.java
@@ -1,11 +1,14 @@
 package com.example.carinfosystem.controller;
 
 import com.example.carinfosystem.model.Branch;
+import com.example.carinfosystem.model.City;
 import com.example.carinfosystem.repository.BranchRepository;
+import com.example.carinfosystem.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/branches")
@@ -14,9 +17,8 @@ public class BranchController {
     @Autowired
     private BranchRepository branchRepository;
 
-    public List<Branch> getAll(){
-        return branchRepository.findAll();
-    }
+    @Autowired
+    private CityRepository cityRepository;
 
     @GetMapping
     public List<Branch> findAll(){
@@ -26,6 +28,12 @@ public class BranchController {
     @GetMapping("/{id}")
     public Branch findById(@PathVariable Long id){
         return branchRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping("/city/{id}")
+    public List<Branch> findByCity(@PathVariable("id") Long cityId){
+        Optional<City> cityOpt = cityRepository.findById(cityId);
+        return cityOpt.map(city -> branchRepository.findByCity(city)).orElse(null);
     }
 
     @PostMapping
--- src/main/java/com/example/carinfosystem/repository/BranchRepository.java
+++ src/main/java/com/example/carinfosystem/repository/BranchRepository.java
@@ -1,9 +1,13 @@
 package com.example.carinfosystem.repository;
 
 import com.example.carinfosystem.model.Branch;
+import com.example.carinfosystem.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BranchRepository extends JpaRepository<Branch, Long> {
+    List<Branch> findByCity(City city);
 }
